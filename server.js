558907d7c287740fe46c74250893ac5095fcd77d
Sufyan Jamleh <s.jamleh@gmail.com>
2020-11-03 14:26:15 +0200

    Sufyan jamleh

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
-
+fs.writeFile('csvData.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  //console.log('Saved!');
+});
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  /*Use the url module to turn the querystring into an object:*/
+  var q = url.parse(req.url, true).query;
+  /*Return the year and month from the query object:*/
+  var txt = q.year + " " + q.month;
+  res.end(txt);
+}).listen(8080);
---
+++ test.js
@@ -0,0 +1,3 @@
+exports.sayHi=function(a){
+    return "hi "+a;
+}
